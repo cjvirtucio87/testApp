43766ec6fecb815be8cd2ff95f283e2b0f14b67e
cjvirtucio87 <cjv287@gmail.com>
2016-05-27 08:23:48 -0700

    Added test for authentication.

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @customer = Customer.new(name: 'tester', email: 'tester@testmail.com', password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  test 'should authenticate' do
+    @customer.password = 'torrential'
+    assert_not @customer.valid?
+  end
 end
