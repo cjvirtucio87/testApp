1fba80d9c3115e77bbdc44f29fdb41da09bd4f09
cjvirtucio87 <cjv287@gmail.com>
2016-05-27 09:42:22 -0700

    Edited email regex validation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[a-zA-Z\-]{4,254}@[a-zA-Z\-]{,6}mail\.\w{3}/
+  VALID_EMAIL_REGEX = /\A[a-zA-Z\-\.]{4,254}@[a-zA-Z\-]{,6}mail\.\w{3}/
   validates :name, :email, presence: true
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
