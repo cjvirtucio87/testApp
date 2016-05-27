2d8c86f28c62c7c4f0ce8bec4266ac7f0f14319c
cjvirtucio87 <cjv287@gmail.com>
2016-05-27 09:53:08 -0700

    Fixed test for authentication.

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -5,8 +5,15 @@ class CustomerTest < ActiveSupport::TestCase
     @customer = Customer.new(name: 'tester', email: 'tester@testmail.com', password: 'foobar', password_confirmation: 'foobar')
   end
 
+  def
+
   test 'should authenticate' do
-    @customer.password = 'torrential'
+    @customer.password = 'foobar'
+    assert @customer.valid?
+  end
+
+  test 'should not authenticate for wrong password input' do
+    @customer.password = 'torrentz'
     assert_not @customer.valid?
   end
 end
