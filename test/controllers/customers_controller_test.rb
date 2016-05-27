07845ede42ed36b400bb5430bf21f706b8e0d721
cjvirtucio87 <cjv287@gmail.com>
2016-05-27 08:59:39 -0700

    Removed unnecessary tests.

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class CustomersControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
+  def setup
+    @customer = Customer.new(id: 1, name: 'tester', email: 'test@testmail.com', 
+                                                    password: 'testword',
+                                                    password_confirmation: 'testword')
+  end
+
+  test "should get index" do
+    get :index
     assert_response :success
   end
 
@@ -11,19 +17,5 @@ class CustomersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
 
 end
