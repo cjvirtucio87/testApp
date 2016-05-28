4a5db3fb1ecd210c735f1742ef766e313af03202
cjvirtucio87 <cjv287@gmail.com>
2016-05-27 17:07:30 -0700

    Added test for successful login.

--- test/integration/customers_login_test.rb
+++ test/integration/customers_login_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class CustomersLoginTest < ActionDispatch::IntegrationTest
+  test "login successfully" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: 'virtucio.cj@gmail.com', password: 'foobar' }
+    assert_template 'sessions/new'
+  end
+
   test "login with an invalid password" do
     get login_path
     assert_template 'sessions/new'
@@ -9,5 +16,5 @@ class CustomersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
-  end  
+  end
 end
