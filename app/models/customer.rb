01eadfa4bb36b294ea9bea94d51f2129bcb0f180
cjvirtucio87 <cjv287@gmail.com>
2016-05-27 21:32:49 -0700

    Added new_token class method for customers model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,9 @@ class Customer < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  #Returns a token. Token is a string of random chars.
+  def Customer.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
