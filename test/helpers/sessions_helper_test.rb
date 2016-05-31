da42cbda94df619003bfe64960793711bd032474
cjvirtucio87 <cjv287@gmail.com>
2016-05-31 14:58:24 -0700

    Added test for mismatched remember _token and _digest.

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -10,4 +10,9 @@ class SessionsHelperTest < ActionView::TestCase
     assert_equal @customer, current_customer
     assert is_logged_in?
   end
+
+  test 'current_customer returns nil when remember _token and _digest mismatch ' do
+    @customer.update_attribute(:remember_digest,Customer.digest(Customer.new_token))
+    assert_nil current_customer
+  end
 end
