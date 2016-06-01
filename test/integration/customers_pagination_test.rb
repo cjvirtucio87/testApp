3dba2d742711688472cf7c6c8b38ce897574843e
cjvirtucio87 <cjv287@gmail.com>
2016-06-01 15:45:59 -0700

    Added pagination test.

---
+++ test/integration/customers_pagination_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CustomersPaginationTest < ActionDispatch::IntegrationTest
+  def setup
+    @customer = customers(:test_customer)
+  end
+
+  test 'all including pagination' do
+    log_in_as(@customer)
+    get customers_all_path
+    assert_template 'customers/all'
+    assert_select 'div.pagination'
+    Customer.paginate(page: 1).each do |customer|
+      assert_select 'a[href=?]', customer_path(customer), text: customer.name
+    end
+  end
+end
