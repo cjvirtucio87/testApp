d2f74fbef6bc41f6c78737e77e7f7f8f9969179c
cjvirtucio87 <cjv287@gmail.com>
2016-06-01 07:10:58 -0700

    Added test for editting different customer's profile.

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CustomersControllerTest < ActionController::TestCase
   def setup
     @customer = customers(:test_customer)
+    @foobar = customers(:foobar)
   end
 
   test "should redirect to login when not logged in" do
@@ -15,5 +16,12 @@ class CustomersControllerTest < ActionController::TestCase
     patch :update, id: @customer, customer: { name: @customer.name, email: @customer.email }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "should redirect to root when updating a different customer's profile" do
+    log_in_as(@customer, { password: 'foobar' })
+    get :edit, id: @foobar
+    assert_not flash.empty?
+    assert_redirected_to root_url
   end
 end
