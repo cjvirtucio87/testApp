92313f5b525180680f1d23a099708dd13eb0221d
cjvirtucio87 <cjv287@gmail.com>
2016-05-31 20:06:04 -0700

    Added acceptance test for successful edit.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -13,4 +13,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                                 password_confirmation: 'meh' }
     assert_template 'customers/edit'
   end
+
+  test 'successful edit should redirect to profile page' do
+    log_in_as(@customer, { password: 'foobar' })
+    get edit_customer_path(@customer)
+    assert_template 'customers/edit'
+    patch customer_path(@customer), customer: { name: 'Christopher',
+                                                email: 'cjv287@gmail.com',
+                                                password: '',
+                                                password_confirmation: '' }
+    assert_not flash.empty?
+    assert_redirected_to @customer
+    # @customer.reload
+    # assert_equal 'CJ', @customer.name
+    # assert_equal 'cjv287@gmail.com', @customer.email
+  end
 end
