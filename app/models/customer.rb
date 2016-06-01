2a1d0b29fa85508a53b3ebe4788bca2b20e4fdcb
cjvirtucio87 <cjv287@gmail.com>
2016-05-31 21:24:05 -0700

    Added allow_nil for pw validation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,7 +7,7 @@ class Customer < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   #Returns a token. Token is a string of random chars.
   def Customer.new_token
