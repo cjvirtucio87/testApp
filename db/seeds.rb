fb5bf91b2744b9c16c5a1b040367e182e682cb6c
cjvirtucio87 <cjv287@gmail.com>
2016-06-01 19:57:39 -0700

    Reset migration due to admin column.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Customer.create!(name: 'CJ',
+                 email: 'virtucio.cj@gmail.com',
+                 password: 'foobar',
+                 password_confirmation: 'foobar',
+                 admin: true)
+
 99.times do |n|
   name = Faker::Name.name
   email = "email-#{n}@gmail.com"
